a0d842f918c0e810d378162b0c9c4541e5d44e0a
joxiah <jmnyarega@gmail.com>
2020-08-31 18:58:28 +0300

    setup theme colors

---
+++ src/theme.css.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  black: "rgb(0,0,0)",
+  light: "rgb(255,255,255)",
+  accent: "rgb(71,68,68)",
+  darkAccent: "rgb(54,54,54)",
+  lightAccent: "rgb(242,242,242)",
+  gray: "rgb(130,130,130)",
+  darkGray: "rgb(79,79,79)",
+};
