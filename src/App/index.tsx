4a5c622c564d7a253f4c14cff91d0c58c5ed07a4
joxiah <jmnyarega@gmail.com>
2020-08-31 19:43:17 +0300

    header

---
+++ src/App/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header header__text-color">
+        <div className="logo">Radio.radio</div>
+        <nav className="nav">
+          <li className="nav-list">Discover</li>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
