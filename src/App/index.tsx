20495e4185d2694519bcb4531674e0e6979fd601
joxiah <jmnyarega@gmail.com>
2020-09-01 10:39:29 +0300

    background image

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../Navbar/index";
+import Home from "../Home";
 
 function App() {
   return (
     <div className="container">
       <NavBar />
+      <Home />
     </div>
   );
 }
