b0a8448a24e5c753db97010bc9d86514f4e41a45
joxiah <jmnyarega@gmail.com>
2020-09-04 13:37:57 +0300

    top 5

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import top5 from "../data/top.json";
 import Disc from "../Common/disc";
 import "./index.css";
 
@@ -14,49 +15,40 @@ const Home = () => (
           </div>
           <div className="mostlistened">
             <ul className="mostlistened-container">
-              <li className="mostlistened-container-list link">
-                <span className="mostlistened__number">01</span>
-                <span className="mostlistened__play play"></span>
-                <span className="mostlistened__radio-name">Radio one</span>
-                <span className="mostlistened__country">Russian</span>
-                <span className="mostlistened__rate">12 kbps</span>
-              </li>
-              <li className="mostlistened-container-list link">
-                <span className="mostlistened__mostlistened__number">02</span>
-                <span className="mostlistened__play play"></span>
-                <span className="mostlistened__radio-name">Radio one</span>
-                <span className="mostlistened__country">Russian</span>
-                <span className="mostlistened__rate">12 kbps</span>
-              </li>
-              <li className="mostlistened-container-list link">
-                <span className="mostlistened__number">03</span>
-                <span className="mostlistened__play play"></span>
-                <span className="mostlistened__radio-name">Radio one</span>
-                <span className="mostlistened__country">Russian</span>
-                <span className="mostlistened__rate">12 kbps</span>
-              </li>
-              <li className="mostlistened-container-list link">
-                <span className="mostlistened__number">04</span>
-                <span className="mostlistened__play play"></span>
-                <span className="mostlistened__radio-name">Radio one</span>
-                <span className="mostlistened__country">Russian</span>
-                <span className="mostlistened__rate">12 kbps</span>
-              </li>
-              <div className="row mostlistened-row__line">
-                <li className="mostlistened-container-list link">
-                  <span className="mostlistened__number">04</span>
-                  <span className="mostlistened__play play"></span>
-                  <span className="mostlistened__radio-name">Radio one</span>
-                  <span className="mostlistened__country">Russian</span>
-                  <span className="mostlistened__rate">12 kbps</span>
-                </li>
-                <Link
-                  to="/discover"
-                  className="mostlistened__line d-center row"
-                >
-                  →
-                </Link>
-              </div>
+              {top5.map((x, i) => (
+                <div className="mostlistened-row__line">
+                  <Link
+                    to={(location) => ({
+                      ...location,
+                      pathname: "/play",
+                      data: x,
+                    })}
+                    className="mostlistened-container-list link"
+                  >
+                    <span className="mostlistened__number mostlistened__item">
+                      {i + 1}
+                    </span>
+                    <span className="mostlistened__play play mostlistened__item"></span>
+                    <span className="mostlistened__radio-name mostlistened__item">
+                      {x.name}
+                    </span>
+                    <span className="mostlistened__country mostlistened__item">
+                      {x.country}
+                    </span>
+                    <span className="mostlistened__rate mostlistened__item">
+                      {x.bitrate}kbps
+                    </span>
+                  </Link>
+                  {i === 4 && (
+                    <Link
+                      to="/discover"
+                      className="mostlistened__line d-center row"
+                    >
+                      →
+                    </Link>
+                  )}
+                </div>
+              ))}
             </ul>
           </div>
         </div>
