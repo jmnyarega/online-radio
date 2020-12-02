00912e817336e4f487de374bd76d746b11c6df70
josiah <jmnyarega@gmail.com>
2020-12-02 21:35:09 +0300

    integrate

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import top5 from "../data/top.json";
 import Disc from "../Common/disc";
 import "./index.css";
 
@@ -18,9 +17,6 @@ const Home = (): any => (
             that is being shared on radio; news, music, way of living, politics,
             etc. Radio.radio is allowing you to experience and understand
             different cultures
-            <span role="img" aria-label="img">
-              🍿
-            </span>
           </p>
           <Link to="/discover" className="btn btn-hero hero-btn">
             Discover
@@ -32,50 +28,14 @@ const Home = (): any => (
             <h4 className="title title__small col">Most listened Stations</h4>
           </div>
           <div className="mostlistened">
-            <ul className="mostlistened-container">
-              {top5.map((x, i) => (
-                <div className="mostlistened-row__line">
-                  <Link
-                    to={(location) => ({
-                      ...location,
-                      pathname: "/play",
-                      data: x,
-                    })}
-                    className="mostlistened-container-list link"
-                    key={i}
-                  >
-                    <span
-                      key={i + 1}
-                      className="mostlistened__number mostlistened__item"
-                    >
-                      {i + 1}
-                    </span>
-                    <span
-                      key={i + 2}
-                      className="mostlistened__play play mostlistened__item"
-                    ></span>
-                    <span
-                      key={i + 3}
-                      className="mostlistened__radio-name mostlistened__item"
-                    >
-                      {x.name}
-                    </span>
-                    <span
-                      key={i + 4}
-                      className="mostlistened__country mostlistened__item"
-                    >
-                      {x.country}
-                    </span>
-                    <span
-                      key={i + 5}
-                      className="mostlistened__rate mostlistened__item"
-                    >
-                      {x.bitrate}kbps
-                    </span>
-                  </Link>
-                </div>
-              ))}
-            </ul>
+            {[0, 2, 6, 8].map((x) => (
+              <iframe
+                title={`home radio ${x}`}
+                src={`https://tunein.com/embed/player/s2972${x}/`}
+                scrolling="no"
+                style={{ width: "100%", height: "100px" }}
+              />
+            ))}
           </div>
         </div>
       </div>
